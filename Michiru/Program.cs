bccb4fe02f9deba9d97b2178f8aef5cd385d82ce
Khamraj Rohit <Rohit.Khamraj@gmail.com>
2017-05-22 21:44:57 -0400

    Add project files.

---
+++ Michiru/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Michiru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
